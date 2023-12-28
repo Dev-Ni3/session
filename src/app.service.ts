02e0059867d7e8a59f2ba7f4ed0cc8dc38fc7768
nitin sain <nitin.sain@1finance.co.in>
2023-12-28 16:25:40 +0530

    add:put patch delete http method

--- src/app.service.ts
+++ src/app.service.ts
@@ -9,4 +9,16 @@ export class AppService {
   postHello():string{
     return 'Post Hello World!';
   }
+
+  patchHello():string{
+    return 'patch Hello World!';
+  }
+
+  putHello():string{
+    return 'put Hello World!';
+  }
+
+  deleteHello():string{
+    return 'delete Hello World!';
+  }
 }
