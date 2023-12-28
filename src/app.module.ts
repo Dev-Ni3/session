9f1c6d3f6ad1aef0420097839e84ed7fe0749899
nitin sain <nitin.sain@1finance.co.in>
2023-12-28 17:02:03 +0530

    add:product order invoices

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { ProductController } from './product/product.controller';
+import { ProductService } from './product/product.service';
+import { OrderModule } from './order/order.module';
+import { InvoicesModule } from './invoices/invoices.module';
 
 @Module({
-  imports: [],
-  controllers: [UserController,AppController],
-  providers: [AppService],
+  imports: [OrderModule, InvoicesModule],
+  controllers: [UserController,AppController, ProductController],
+  providers: [AppService,UserService, ProductService],
 })
 export class AppModule {}
